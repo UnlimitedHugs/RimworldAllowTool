0abb895918a3573c36e00258f749da73f9e13190
UnlimitedHugs <unlimited.hugs@gmail.com>
2017-11-05 16:13:14 +0400

    "Mine connected" will only spread to matching ore types

--- Source/Context/MenuProvider_Mine.cs
+++ Source/Context/MenuProvider_Mine.cs
@@ -10,7 +10,8 @@ namespace AllowTool.Context {
 	/// Expands Mine designation on tiles to adjacent ore tiles. This is basically a vein miner.
 	/// </summary>
 	public class MenuProvider_Mine : BaseDesignatorMenuProvider {
-		private delegate bool ValidationPredicate(IntVec3 cell, Map map);
+		private delegate bool InitialCandidateFilter(IntVec3 cell, Map map);
+		private delegate bool ExpansionCandidateFilter(IntVec3 fromCell, IntVec3 toCell, Map map);
 
 		public override string EntryTextKey {
 			get { return "Designator_context_mine"; }
@@ -32,18 +33,24 @@ namespace AllowTool.Context {
 				return;
 			}
 			// expand designations, excluding designated fogged cells to prevent exposing completely hidden ores
-			var hits = FloodExpandDesignationType(DesignationDefOf.Mine, map, (cell, m) => !m.fogGrid.IsFogged(cell), CellCanBeMineDesignated);
+			var hits = FloodExpandDesignationType(DesignationDefOf.Mine, map, (cell, m) => !m.fogGrid.IsFogged(cell), MineDesignationExpansionIsValid);
 			Messages.Message("Designator_context_mine_succ".Translate(hits), MessageTypeDefOf.TaskCompletion);
 		}
 
-		private bool CellCanBeMineDesignated(IntVec3 cell, Map map) {
-			var thing = map.edificeGrid[cell];
-			return thing != null && thing.def.building != null && thing.def.mineable && thing.def.building.isResourceRock;
+		private bool MineDesignationExpansionIsValid(IntVec3 cellFrom, IntVec3 cellTo, Map map) {
+			var oreFrom = TryGetMineableAtPos(cellFrom, map);
+			var oreTo = TryGetMineableAtPos(cellTo, map);
+			return oreFrom != null && oreTo != null && oreFrom.def == oreTo.def;
+		}
+
+		private Thing TryGetMineableAtPos(IntVec3 pos, Map map) {
+			var thing = map.edificeGrid[pos];
+			return thing != null && thing.def.building != null && thing.def.mineable && thing.def.building.isResourceRock ? thing : null;
 		}
 
 		// ensure all selected ores are Mine designated
 		private void MineDesignateSelectedOres(Map map) {
-			var toDesignate = Find.Selector.SelectedObjects.OfType<Thing>().Where(o => CellCanBeMineDesignated(o.Position, map));
+			var toDesignate = Find.Selector.SelectedObjects.OfType<Thing>().Where(o => TryGetMineableAtPos(o.Position, map) != null);
 			foreach (var thing in toDesignate) {
 				thing.Position.ToggleDesignation(DesignationDefOf.Mine, true);
 			}
@@ -51,7 +58,7 @@ namespace AllowTool.Context {
 
 		// Expands cell designations iteratively to adjacent cells that are deemed valid by the expansionFilter. 
 		// Returns the number of additional cells designated.
-		private int FloodExpandDesignationType(DesignationDef designationDef, Map  map, ValidationPredicate initialFilter, ValidationPredicate expansionFilter) {
+		private int FloodExpandDesignationType(DesignationDef designationDef, Map  map, InitialCandidateFilter initialFilter, ExpansionCandidateFilter expansionFilter) {
 			var designatedCells = map.designationManager.SpawnedDesignationsOfDef(designationDef).Where(d => !d.target.HasThing).Select(d => d.target.Cell).ToList();
 			var markedCells = new HashSet<IntVec3>(designatedCells);
 			var cellsToProcess = new Queue<IntVec3>(designatedCells.Where(c => initialFilter(c, map)));
@@ -63,7 +70,7 @@ namespace AllowTool.Context {
 				var baseCell = cellsToProcess.Dequeue();
 				for (int i = 0; i < adjacent.Length; i++) {
 					var cell = baseCell + adjacent[i];
-					if (!markedCells.Contains(cell) && expansionFilter(cell, map)) {
+					if (!markedCells.Contains(cell) && expansionFilter(baseCell, cell, map)) {
 						map.designationManager.AddDesignation(new Designation(cell, designationDef));
 						markedCells.Add(cell);
 						hitCount++;
