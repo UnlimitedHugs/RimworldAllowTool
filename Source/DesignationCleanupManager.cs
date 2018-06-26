3a3547dc27df58cba34b069749f185c18ec8e7b3
UnlimitedHugs <unlimited.hugs@gmail.com>
2018-06-26 20:27:08 +0400

    Auto-cleanup for expired urgent hauling and rearming designations

--- Source/DesignationCleanupManager.cs
+++ Source/DesignationCleanupManager.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using RimWorld;
 using Verse;
 
 namespace AllowTool {
@@ -6,35 +7,39 @@ namespace AllowTool {
 	/// Periodically cleans up designations that no longer have valid targets.
 	/// </summary>
 	public static class DesignationCleanupManager {
-		private const int TickInterval = 60;
+		private const int TickInterval = 30;
+
+		private static readonly Queue<Designation> cleanupList = new Queue<Designation>();
 
 		public static void Tick(int currentTick) {
-			if (currentTick%TickInterval != 0) return;
 			if (Current.Game == null || Current.Game.Maps == null) return;
-			CleanupFinishOffDesignations();
-		}
-
-		private static void CleanupFinishOffDesignations() {
-			var maps = Find.Maps;
-			List<Designation> cleanupList = null;
+			var maps = Current.Game.Maps;
+			var tickStep = currentTick % TickInterval;
 			for (int i = 0; i < maps.Count; i++) {
 				var map = maps[i];
-				if(map.designationManager == null) continue;
-				var mapDesignations = map.designationManager.allDesignations;
-				for (int j = 0; j < mapDesignations.Count; j++) {
-					var des = mapDesignations[j];
-					if (des.def == AllowToolDefOf.FinishOffDesignation && !Designator_FinishOff.IsValidDesignationTarget(des.target.Thing)) {
-						if (cleanupList == null) {
-							cleanupList = new List<Designation>();
-						}
-						cleanupList.Add(des);
-					}
+				// distribute maps on the time spectrum for better performance
+				if (tickStep == map.uniqueID % TickInterval) {
+					CleanupDesignations(map);
 				}
 			}
-			if (cleanupList != null) {
-				foreach (var designation in cleanupList) {
-					designation.designationManager.RemoveDesignation(designation);
+		}
+
+		private static void CleanupDesignations(Map map) {
+			if(map.designationManager == null) return;
+			var mapDesignations = map.designationManager.allDesignations;
+			for (int i = 0; i < mapDesignations.Count; i++) {
+				var des = mapDesignations[i];
+				var desThing = des.target.Thing;
+				if (des.target.Thing == null 
+					|| (des.def == AllowToolDefOf.FinishOffDesignation && !Designator_FinishOff.IsValidDesignationTarget(des.target.Thing))
+					|| (des.def == AllowToolDefOf.HaulUrgentlyDesignation && desThing.IsInValidBestStorage())
+					|| des.def == AllowToolDefOf.RearmUrgentlyDesignation && (!(desThing is Building_TrapRearmable) || (desThing as  Building_TrapRearmable).Armed)){
+					cleanupList.Enqueue(des);
 				}
+			}
+			while (cleanupList.Count > 0) {
+				var des = cleanupList.Dequeue();
+				des.designationManager.RemoveDesignation(des);
 			}
 		}
 	}
