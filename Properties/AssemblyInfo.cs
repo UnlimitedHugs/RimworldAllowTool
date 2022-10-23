59ff818ca8c2117bba6d6fba4d9daf0a84be19b1
UnlimitedHugs <unlimited.hugs@gmail.com>
2022-10-23 21:12:31 +0400

    Version up

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.6.0")]
-[assembly: AssemblyFileVersion("3.11.1")]
+[assembly: AssemblyFileVersion("3.12.0")]
